126f89489717297e0109bb0797abc0b3309073d5
johntechcomputing <johntechcomputing@gmail.com>
2023-12-14 19:35:02 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a line with puts function
+ * Reaturn: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a mulitilingual puzzle");
+return (0);
+}
