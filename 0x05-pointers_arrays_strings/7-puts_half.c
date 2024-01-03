5c4a920f510631630fc6b73213fb5dc75dbdbf3c
jtechycomputing <jtechycomputing@gmail.com>
2024-01-03 12:23:26 +0300

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,26 +6,14 @@
  */
 void puts_half(char *str)
 {
-	int j = 0;
-	int k;
+int i;
+int k;
+int count = 0;
 
-	while (str[j] != '\0')
-	{
-	j++;
-	}
-	if (j % 2 == 1)
-	{
-	k = (j - 1) / 2;
-	k += 1;
-	}
-	else
-	{
-	k = j / 2;
-	}
-
-	for (; k < j; k++)
-	{
-	_putchar(str[k]);
-	}
-	_putchar(;\n');
+for (i = 0; str[i] != '\0'; i++)
+	count++;
+	k = (count - 1) / 2;
+for (i = k + 1; str[i] != '\0'; i++)
+	_putchar(str[i]);
+	_putcahr('\n');
 }
