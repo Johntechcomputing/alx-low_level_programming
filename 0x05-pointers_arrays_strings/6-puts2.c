7425372e1f9a48c79bcec7fc27422409d65fd9c5
jtechycomputing <jtechycomputing@gmail.com>
2024-01-03 10:51:25 +0300

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,17 +7,12 @@
  */
 void puts2(char *str)
 {
-	int i;
-	int j = 0;
+int i;
 
-	while (str[j] != '\0')
-	{
-	j++;
-	}
-
-	for (i = 0 ; i <= j ; i += 2)
-	{
-	_putchar(str[i]);
-	}
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (i % 2 == 0)
+		_putchar(str[i]);
+}
 	_putchar('\n');
 }
