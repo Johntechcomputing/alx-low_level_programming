06a1712204081972cb765b9a04c084324ac8ecf1
jtechycomputing <jtechycomputing@gmail.com>
2024-01-11 21:30:07 +0300

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,7 @@ void _puts_recursion(char *s);
 void _print_rev_recustion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
-int _pow_recursion(int x. int y);
+int _pow_recursion(int x, int y);
 int sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
