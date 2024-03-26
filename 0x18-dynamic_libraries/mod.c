4ee0c5d3b60189410313c809c6d77cdf3e4deaf5
Johntechcomputing <johntechcomputing@gmail.com>
2024-03-26 03:24:38 +0300

    first advanced

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mod - modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
